bc94bc60e77c5dcd8ca1a4f2ebe67be8ddbf9b03
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-03-14 18:01:33 -0300

    concluindo

--- js/script.js
+++ js/script.js
@@ -20,16 +20,83 @@ function verificar() {
 
         if (fsex[0].checked) { // se oque tiver marcado for o fsex 0, é sinal que é masculino
             gênero = 'Homem'
-            if (idade >= 0 && idade < 10) {
+            if (idade >= 0 && idade <= 0 && idade <= 0) {
+                // bebe1
+                img.setAttribute('src', './img/homem-bebe1.png')
+            } else if (idade >= 1 && idade <= 1) {
+                // bebe 2
+                img.setAttribute('src', './img/homem-bebe2.png')
+            } else if (idade >= 2 && idade < 8) {
+                // bebe 3 
+                img.setAttribute('src', './img/homem-bebe3.png')
+            } else if (idade < 13) {
                 // criança
-            } else if (idade >= 10 && idade < 21) {
-
+                img.setAttribute('src', './img/homem-crianca.png')
+            } else if (idade < 18) {
+                // adolescente
+                img.setAttribute('src', './img/homem-adolescente.png')
+            }  else if (idade < 24) {
+                // jovem adulto
+                img.setAttribute('src', './img/homem-jovem-adulto.png')
+            } else if (idade < 35) {
+                // adulto1
+                img.setAttribute('src', './img/homem-adulto1.png')
+            } else if (idade < 48) {
+                // adulto2
+                img.setAttribute('src', './img/homem-adulto2.png')
+            } else if (idade < 68) {
+                // velho1
+                img.setAttribute('src', './img/homem-velho1.png')
+            } else if (idade < 122){ // se não for nem entre 0 e 68 é esse
+                //velho 2
+                img.setAttribute('src', './img/homem-velho2.png')
+            } else {
+                //morto
+                img.setAttribute('src', './img/anjo.png')
             }
-            
+
+
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
+
+            if (idade >= 0 && idade <= 0) {
+                // bebe1
+                img.setAttribute('src', './img/mulher-bebe1.png')
+            } else if (idade >= 1 && idade <= 1) {
+                // bebe 2
+                img.setAttribute('src', './img/mulher-bebe2.png')
+            } else if (idade >= 2 && idade < 8) {
+                // bebe 3 
+                img.setAttribute('src', './img/mulher-bebe3.png')
+            } else if (idade < 13) {
+                // criança
+                img.setAttribute('src', './img/mulher-crianca.png')
+            } else if (idade < 18) {
+                // adolescente
+                img.setAttribute('src', './img/mulher-adolescente.png')
+            }  else if (idade < 24) {
+                // jovem adulto
+                img.setAttribute('src', './img/mulher-jovem-adulta.png')
+            } else if (idade < 35) {
+                // adulto1
+                img.setAttribute('src', './img/mulher-adulta1.png')
+            } else if (idade < 48) {
+                // adulto2
+                img.setAttribute('src', './img/mulher-adulta2.png')
+            } else if (idade < 68) {
+                // velho1
+                img.setAttribute('src', './img/mulher-velha1.png')
+            } else if (idade < 122){ // se não for nem entre 0 e 68 é esse
+                //velho 2
+                img.setAttribute('src', './img/mulher-velha2.png')
+            } else {
+                //morto
+                img.setAttribute('src', './img/anjo.png')
+            }
         }
+
         res.getElementsByClassName.textAling = 'center' // text-align pelo js
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img) // adicina um elemento, que no caso é o img 
     }
 }
